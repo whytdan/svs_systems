c7f8ad9e14aac5d3af08aa17e73eff1ba84bf4b7
whytdan <aktan.r.a@gmail.com>
2021-03-14 16:43:08 +0600

    rebuild front app

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#!/bin/sh
+
 until cd /app/backend/src
 do
     echo "Waiting for server volume..."
@@ -14,3 +16,4 @@ done
 ./manage.py collectstatic --noinput
 
 gunicorn src.wsgi --bind 0.0.0.0:8000 --workers 2 --threads 2
+
