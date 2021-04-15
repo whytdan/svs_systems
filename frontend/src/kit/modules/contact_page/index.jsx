9d8e02594bea440eb5c67a244472883d4c7ab5a5
whytdan <aktan.r.a@gmail.com>
2021-04-15 12:57:25 +0600

    build app and change settings

--- frontend/src/kit/modules/contact_page/index.jsx
+++ frontend/src/kit/modules/contact_page/index.jsx
@@ -8,8 +8,8 @@ export default function ContactPage(props) {
   return (
     <Layout bottom_header={false}>
       <main>
-        <Contacts />
         <ContactForm />
+        <Contacts />
         <TwoGisMap/>
       </main>
     </Layout>
