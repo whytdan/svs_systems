28fdae1345e2f262f6b26e918b255abe091c12af
whytdan <aktan.r.a@gmail.com>
2021-04-17 11:10:59 +0600

    conf fixes

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'svs',
-        'USER': 'svs',
-        'PASSWORD': 'svs',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'svs',
+#         'USER': 'svs',
+#         'PASSWORD': 'svs',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 
 REST_FRAMEWORK = {
